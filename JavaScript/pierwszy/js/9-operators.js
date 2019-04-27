da7f8f3485813770c30467f5126f3d9e397b8ab6
Natalia Kaminska <natalia.katarzyna.kaminska@gmail.com>
2019-04-27 14:55:31 +0200

    operators

---
+++ JavaScript/pierwszy/js/9-operators.js
@@ -0,0 +1,30 @@
+'use strict'
+
+let liczba1 = 8, liczba2 = 3;
+
+let wynik;
+
+wynik = liczba1 % liczba2;
+console.log(wynik);
+
+
+liczba1 = liczba1 + 100; //w skrocie liczba+= 100;
+liczba1 += 200;
+console.log(liczba1);
+
+if ("2" === 2) {
+    console.log("prawda");
+} else {
+    console.log("Nieprawda");
+}
+
+while (liczba2 < liczba1) {
+    console.log
+}
+
+if ( !(("2" !==2) && (8 < 4))) {
+    document.write("<br> prawda");
+}
+let wynikLogiczny = (3>5) ? "prawda" : "falsz";
+
+console.log(wynikLogiczny);
