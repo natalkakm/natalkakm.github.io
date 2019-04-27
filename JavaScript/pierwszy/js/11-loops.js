45ed3d3c0cf530e6928f72e1d44187f72cb23b45
Natalia Kaminska <natalia.katarzyna.kaminska@gmail.com>
2019-04-27 16:04:27 +0200

    loops

---
+++ JavaScript/pierwszy/js/11-loops.js
@@ -0,0 +1,46 @@
+'use strict';
+
+
+// Pętla for - najprostsza ever ( wyrazenie poczatkowe; wyrazenie warunkowe; wyrazenie modyfikujace)
+
+for ( let i=10 ; i>0 ; i--) {
+    console.log(i);
+
+}
+
+//Pętla forEach
+
+let tablica = [ "Krystian", "Monika", "Danuta" ];
+
+for (let i=0; i < tablica.length; ++i ) {
+    console.log(tablica[i]);
+}
+
+const iteruj = ( element, index ) => {
+    console.log( "Element z Indexem: " + index + " ma wartosc" + element);
+}
+
+tablica.forEach( (element, index) => {
+    console.log( "Element z Indexem: " + index + " ma wartosci " + element);
+} );
+
+// Petla for in
+
+let person = {
+    name: 'Krystian',
+    age: 35
+}
+
+for(let key in person) {
+    console.log(person[key]);
+}
+
+
+// petla for of
+
+let iterable = [10, 10, 30];
+
+for (let value of iterable) {
+    value += 1;
+    console.log(value);
+}
