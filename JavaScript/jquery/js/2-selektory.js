edd970c51ae8fdd6e29d4408d9698d3e02978bea
Natalia Kaminska <natalia.katarzyna.kaminska@gmail.com>
2019-05-11 09:55:57 +0200

    jquery selektory

---
+++ JavaScript/jquery/js/2-selektory.js
@@ -0,0 +1,10 @@
+let naglowek1 = $('#naglowek-1');
+console.log(naglowek1);
+
+naglowek1.css('color', 'red');
+
+let paragrafy = $('p');
+console.log(paragrafy);
+
+let parByClass = $('.par');
+console.log(parByClass);
