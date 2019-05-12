6806213ed3f182e84562288e394867d11bdec16f
Natalia Kaminska <natalia.katarzyna.kaminska@gmail.com>
2019-05-12 10:42:57 +0200

    ajax

---
+++ JavaScript/ajax/js/main.js
@@ -0,0 +1,54 @@
+// fetch('https://jsonplaceholder.typicode.com/posts') 
+//     .then(response => response.json())
+//     .then(response => {
+// console.log(response); });
+
+//największym uproszczeniem jest fetch, więc na tym się skupię
+
+
+// $.getJSON(
+//     'https:jsonplaceholder.typicode.com/posts',
+//     response => {
+//         console.log(response);
+//     }
+// )
+
+//z JSONa lepiej korzystać, jeśli i tak się z niego korzysta
+
+// $.ajax({
+//     url: "https://jsonplaceholder.typicode.com/users",
+//     dataType: "json",
+//     success: function (resultJSON) { 
+//         console. log(resultJSON);
+//     },
+//     onerror: function (msg) { 
+//         console. log(msg);
+//     } 
+// });
+
+const ajax = (method, url) => {
+    console.log(method, url);
+    //tworzymy obiekt
+    let httpReq = new XMLHttpRequest();
+
+    //otwieramy połączenie
+    httpReq.open(method, url);
+    // console.log(httpReq);
+    //w momencie dotarcia do serwera
+    httpReq.onreadystatechange = () => {
+        //jesli wszystko poszlo pomyslnie i dane zostały zwrócone
+        if (httpReq.readyState === 4 && httpReq.status === 200) {
+            //wrzucamy dane do zmiennej
+            let response = httpReq.responseText;
+            console.log(response);
+
+            //zerujemy połączenie, czyli rozłączamy się z serwerem
+            httpReq = null;
+
+        }
+    }
+    //wywolujemy onreadystatechange()
+    httpReq.send();
+}
+
+ajax('GET', 'https://jsonplaceholder.typicode.com/users');
